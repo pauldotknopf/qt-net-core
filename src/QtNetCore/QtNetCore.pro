bb61c78265fb72828625e2910af05ef194c27fca
Paul Knopf <pauldotknopf@gmail.com>
2017-05-06 20:29:09 -0400

    Got the linking all setup.

--- src/QtNetCore/QtNetCore.pro
+++ src/QtNetCore/QtNetCore.pro
@@ -3,3 +3,5 @@ TEMPLATE = subdirs
 SUBDIRS += \
     QtNetCoreEngine \
     QtNetCoreSandbox
+
+QtNetCoreSandbox.depends = QtNetCoreEngine
--- src/QtNetCore/QtNetCoreSandbox/main.cpp
+++ src/QtNetCore/QtNetCoreSandbox/main.cpp
@@ -1,8 +1,14 @@
 #include <QCoreApplication>
+#include <iostream>
+#include "qtnetcoreengine.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    new QtNetCoreEngine();
+
+    std::cout << "test\n";
 
     return a.exec();
 }
